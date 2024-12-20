e4b29f06a19f81ebe3077753eec1eb57b5007419
Nikita Semenov <nasemenov726@gmail.com>
2024-12-20 16:38:08 +0300

    feat (models/tests) init test_notification; init some models

--- bot/models.py
+++ bot/models.py
@@ -1,8 +1,9 @@
 import datetime
 
 import sqlalchemy as sa
-from sqlalchemy import BigInteger, UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.ext.associationproxy import association_proxy, AssociationProxy
 
 from core.db import Base
 
@@ -19,3 +20,31 @@ class UserModel(Base):
     registered_at: Mapped[datetime.datetime] = mapped_column(server_default=sa.func.now())
     is_admin: Mapped[bool] = mapped_column(default=False)
     email: Mapped[str | None]
+    services: Mapped[list['ServiceModel']] = relationship(
+
+    )
+
+    service_titles: Mapped[list[str]] = association_proxy(
+        'services',
+        'title'
+    )
+
+
+class ServiceModel(Base):
+    __tablename__ = 'services'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+
+
+class UserServiceAssociation(Base):
+    __tablename__ = 'user_service_associations'
+    
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), 
+        primary_key=True
+    )
+    service_id: Mapped[int] = mapped_column(
+        ForeignKey('services.id'), 
+        primary_key=True
+    )
